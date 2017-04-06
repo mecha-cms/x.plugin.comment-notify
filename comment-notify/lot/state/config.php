1b244103876cfd4e2be676791aeb0dd640ddd05c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-04-07 00:35:32 +0700

    Update

--- comment-notify/lot/state/config.php
+++ comment-notify/lot/state/config.php
@@ -2,8 +2,8 @@
 
 return [
     'page' => [
-        'title' => '%{site.title}% &#x00B7; New Comment', // email subject
-        'email' => 't.nurrohman77@gmail.com', // your email address
+        'title' => '%{site.title}% · New Comment', // email subject
+        'email' => 'email@domain.com', // your email address
         'content' => '<p>A new comment has been added to <a href="%{comment.url}%">%{page.title}%</a>.</p>' // email message
     ]
 ];
