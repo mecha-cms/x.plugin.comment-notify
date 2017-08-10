4394d8fdefbca0c64db5ef651959278311bd1d30
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-10 18:23:57 +0700

    Updates

--- comment-notify/index__.php
+++ comment-notify/index__.php
@@ -7,31 +7,30 @@ function fn_comment_notify($file) {
     global $site, $url;
     // Delay sending the email notice after we create the comment file…
     Hook::set('guardian.kick.before', function($long) use($file, $site, $url) {
-        $comment = new Comment($file);
         $path = Path::F(Path::D($file), COMMENT);
+        $o = [
+            'page' => new Page(File::exist([
+                PAGE . DS . $path . '.page',
+                PAGE . DS . $path . '.archive'
+            ], null)),
+            'comment' => new Comment($file)
+        ];
         $state = Plugin::state(__DIR__);
         $from = Request::post('email');
         $to = $state['page']['email'];
         $subject = __replace__($state['page']['title'], [
-            'site.title' => To::text($site->title)
+            'title' => To::text($site->title)
         ]);
-        $s = File::exist([PAGE . DS . $path . '.page', PAGE . DS . $path . '.archive']);
-        $page = Page::open($s)->get([
-            'title' => '?',
-            'description' => '?'
-        ]);
-        $message = __replace__($state['page']['content'], [
-            'comment.time' => $comment->time,
-            'comment.url' => $comment->url,
-            'comment.id' => $comment->id,
-            'comment.author' => $comment->author . "",
-            'comment.email' => $comment->email,
-            'comment.link' => $comment->link,
-            'comment.status' => $comment->status,
-            'comment.content' => $comment->content,
-            'page.title' => $page['title'],
-            'page.description' => $page['description']
-        ]);
+        foreach ([
+            'comment' => ['author', 'content', 'email', 'id', 'link', 'status', 'time', 'url'],
+            'page' => ['description', 'title']
+        ] as $k => $v) {
+            foreach ($v as $kk => $vv) {
+                $message = __replace__($state['page']['content'], [
+                    $k . '.' . $vv => $o[$k]->{$vv}
+                ]);
+            }
+        }
         if (!empty($to)) {
             Message::send($from, $to, $subject, $message);
         }
--- comment-notify/lot/state/config.php
+++ comment-notify/lot/state/config.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
 
 return [
     'page' => [
-        'title' => '%{site.title}% · New Comment', // email subject
+        'title' => '%{title}% · New Comment', // email subject
         'email' => 'email@domain.com', // your email address
         'content' => '<p style="font-size:150%;">%{comment.author}% » <a href="%{comment.url}%" target="_blank">%{page.title}%</a></p>%{comment.content}%' // email message
     ]
