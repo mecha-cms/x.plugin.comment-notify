9c14d5fa29899f014e9142472c73bcb0ba17cbf3
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-13 18:57:06 +0700

    Updates for @mecha-cms/genome
    + Better embedded variable(s).

--- comment-notify/index__.php
+++ comment-notify/index__.php
@@ -2,35 +2,27 @@
 
 function fn_comment_notify($file) {
     if (Message::$x) {
-        return $file; // There are some error(s) in the form
+        return $file; // There are some error(s) in the form, skip!
     }
-    global $site, $url;
+    global $site, $url, $u_r_l;
     // Delay sending the email notice after we create the comment file…
     Hook::set('guardian.kick.before', function($long) use($file, $site, $url) {
         $path = Path::F(Path::D($file), COMMENT);
         $o = [
+            'comment' => new Comment($file),
             'page' => new Page(File::exist([
                 PAGE . DS . $path . '.page',
                 PAGE . DS . $path . '.archive'
             ], null)),
-            'comment' => new Comment($file)
+            'site' => $site,
+            'url' => $url,
+            'u_r_l' => $u_r_l
         ];
         $state = Plugin::state(__DIR__);
         $from = Request::post('email');
         $to = $state['page']['email'];
-        $subject = __replace__($state['page']['title'], [
-            'title' => To::text($site->title)
-        ]);
-        foreach ([
-            'comment' => ['author', 'content', 'email', 'id', 'link', 'status', 'time', 'url'],
-            'page' => ['description', 'title']
-        ] as $k => $v) {
-            foreach ($v as $kk => $vv) {
-                $message = __replace__($state['page']['content'], [
-                    $k . '.' . $vv => $o[$k]->{$vv}
-                ]);
-            }
-        }
+        $subject = __replace__($state['page']['title'], $o);
+        $message = __replace__($state['page']['content'], $o);
         if (!empty($to)) {
             Message::send($from, $to, $subject, $message);
         }
--- comment-notify/lot/state/config.php
+++ comment-notify/lot/state/config.php
@@ -2,7 +2,7 @@
 
 return [
     'page' => [
-        'title' => '%{title}% · New Comment', // email subject
+        'title' => '%{site.title}% · New Comment', // email subject
         'email' => 'email@domain.com', // your email address
         'content' => '<p style="font-size:150%;">%{comment.author}% » <a href="%{comment.url}%" target="_new">%{page.title}%</a></p>%{comment.content}%' // email message
     ]
