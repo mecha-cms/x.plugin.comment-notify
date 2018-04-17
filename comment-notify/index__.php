e9642c2c27a9f27912046b4584701a823f80e871
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-17 21:13:07 +0700

    Add More Propert(y|ies)

--- comment-notify/index__.php
+++ comment-notify/index__.php
@@ -4,12 +4,14 @@ function fn_comment_notify($file) {
     if (Message::$x) {
         return $file; // There are some error(s) in the form, skip!
     }
-    global $site, $url, $u_r_l;
+    global $date, $site, $url, $u_r_l;
     // Delay sending the email notice after we create the comment file…
     Hook::set('guardian.kick.before', function() use($file, $site, $url, $u_r_l) {
         $path = Path::F(Path::D($file), COMMENT);
         $o = [
             'comment' => new Comment($file),
+            'config' => $site,
+            'date' => $date,
             'page' => new Page(File::exist([
                 PAGE . DS . $path . '.page',
                 PAGE . DS . $path . '.archive'
@@ -18,8 +20,8 @@ function fn_comment_notify($file) {
             'url' => $url,
             'u_r_l' => $u_r_l
         ];
-        $state = Plugin::state(__DIR__);
-        $from = Request::post('email');
+        $state = Plugin::state('comment-notify');
+        $from = HTTP::post('email');
         $to = $state['page']['email'];
         $subject = To::text(__replace__($state['page']['title'], $o));
         $message = To::text(__replace__($state['page']['content'], $o), HTML_WISE_I . ',p', true);
